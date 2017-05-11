acc3cf9fe6ec7b8b4680e0dba22679da9a784565
Adrien Castex <adrien.castex@gmail.com>
2017-05-11 22:17:07 +0200

    Integrated GET method

--- src/server/commands/Get.ts
+++ src/server/commands/Get.ts
@@ -3,6 +3,23 @@ import { IResource } from '../../resource/Resource'
 
 export default function(arg : MethodCallArgs, callback)
 {
-    arg.setCode(HTTPCodes.NotImplemented);
-    callback();
+    arg.getResource((e, r) => {
+        if(e)
+        {
+            arg.setCode(HTTPCodes.NotFound)
+            callback();
+            return;
+        }
+
+        r.read((e, c) => {
+            if(e)
+                arg.setCode(HTTPCodes.NotFound)
+            else
+            {
+                arg.setCode(HTTPCodes.OK);
+                arg.response.write(c);
+            }
+            callback();
+        })
+    })
 }
