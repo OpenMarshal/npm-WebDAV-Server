0369cbd78db777b8aba78a1d94599a1ff140ba8e
Adrien Castex <adrien.castex@gmail.com>
2017-05-12 15:27:24 +0200

    Exported WebDAVServerOptions

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,3 @@
 export { VirtualFile, VirtualFolder, VirtualResource } from './resource/VirtualResource';
+export { WebDAVServer, WebDAVServerOptions } from './server/WebDAVServer';
 export { VirtualFSManager } from './manager/VirtualFSManager';
-export { WebDAVServer } from './server/WebDAVServer';
