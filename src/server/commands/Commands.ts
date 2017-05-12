1d210cedfe3752e575b132e830202ba83e4b7fcf
Adrien Castex <adrien.castex@gmail.com>
2017-05-12 10:45:23 +0200

    Implemented MkCol Method

--- src/server/commands/Commands.ts
+++ src/server/commands/Commands.ts
@@ -1,9 +1,11 @@
 import NotImplemented from './NotImplemented'
 import Propfind from './Propfind'
+import Mkcol from './Mkcol'
 import Get from './Get'
 
 export default {
     NotImplemented : NotImplemented,
     Propfind : Propfind,
-    Get: Get
+    Get: Get,
+    Mkcol : Mkcol
 }
---
+++ src/server/commands/Mkcol.ts
@@ -0,0 +1,42 @@
+import { HTTPCodes, MethodCallArgs, WebDAVRequest } from '../WebDAVRequest'
+import { IResource, ResourceType } from '../../resource/Resource'
+import { FSPath } from '../../manager/FSManager'
+import * as path from 'path'
+
+export default function(arg : MethodCallArgs, callback)
+{
+    arg.server.getResourceFromPath(arg.path.getParent(), (e, r) => {
+        if(e)
+        {
+            console.log(arg.path.toString())
+            arg.setCode(HTTPCodes.NotFound)
+            callback();
+            return;
+        }
+
+        if(!r.fsManager)
+        {
+            arg.setCode(HTTPCodes.InternalServerError)
+            callback();
+            return;
+        }
+        
+        var resource = r.fsManager.newResource(arg.uri, path.basename(arg.uri), ResourceType.Directory, r);
+        resource.create(e => {
+            if(e)
+            {
+                arg.setCode(HTTPCodes.InternalServerError)
+                callback();
+                return;
+            }
+        
+            r.addChild(resource, e => {
+                if(e)
+                    arg.setCode(HTTPCodes.InternalServerError)
+                else
+                    arg.setCode(HTTPCodes.Created)
+                callback();
+            })
+        })
+    })
+}
