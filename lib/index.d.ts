8289c68a45b0233f4101fb33ddebed99133951df
Adrien Castex <adrien.castex@gmail.com>
2017-05-12 10:42:09 +0200

    Exported VirtualFSManager

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,3 @@
 export { VirtualFile, VirtualFolder, VirtualResource } from './resource/VirtualResource';
+export { VirtualFSManager } from './manager/VirtualFSManager';
 export { WebDAVServer } from './server/WebDAVServer';
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 
 export { VirtualFile, VirtualFolder, VirtualResource } from './resource/VirtualResource';
-
+export { VirtualFSManager } from './manager/VirtualFSManager';
 export { WebDAVServer } from './server/WebDAVServer';
