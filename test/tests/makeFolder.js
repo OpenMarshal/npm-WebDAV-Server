681caba7d09ae17821f5b1c89ee0deceb58f3292
Adrien Castex <adrien.castex@gmail.com>
2017-05-13 14:16:09 +0200

    Added tests for the physical folder

--- test/tests/makeFolder.js
+++ test/tests/makeFolder.js
@@ -1,10 +1,12 @@
 var webdav = require('../../lib/index.js'),
-    Client = require("webdav-fs")
+    Client = require("webdav-fs"),
+    path = require('path'),
+    fs = require('fs')
 
 module.exports = (test, options, index) => test('make a folder', isValid =>
 {
     var server = new webdav.WebDAVServer();
-    isValid = isValid.multiple(3, server);
+    isValid = isValid.multiple(4, server);
     server.rootResource.addChild(new webdav.VirtualFile('testFile.txt'), e => {
         if(e)
         {
@@ -18,27 +20,50 @@ module.exports = (test, options, index) => test('make a folder', isValid =>
             "http://127.0.0.1:" + (options.port + index)
         );
         
-        wfs.mkdir('/testFile.txt/testFail', (e, content) => {
+        wfs.mkdir('/testFile.txt/testFail', (e) => {
             isValid(!!e)
         })
 
-        wfs.mkdir('/undefined/testFail', (e, content) => {
+        wfs.mkdir('/undefined/testFail', (e) => {
             isValid(!!e)
         })
 
-        wfs.mkdir('/testSuccess', (e, content) => {
+        wfs.mkdir('/testSuccess', (e) => {
             if(e)
             {
                 isValid(false, e)
                 return;
             }
 
-            wfs.mkdir('/testSuccess/testSuccess2', (e, content) => {
+            wfs.mkdir('/testSuccess/testSuccess2', (e) => {
                 if(e)
                     isValid(false, e)
                 else
                     isValid(true);
             })
         })
+        
+        const folderName = 'makeFolder';
+        const folderPath = path.join(__dirname, folderName);
+
+        const subFolderName = 'folder';
+        const subFolderPath = path.join(folderPath, subFolderName);
+        if(fs.existsSync(subFolderPath))
+            fs.rmdirSync(subFolderPath);
+        
+        server.rootResource.addChild(new webdav.PhysicalFolder(folderPath), e => {
+            if(e)
+            {
+                isValid(false, e)
+                return;
+            }
+
+            wfs.mkdir('/' + folderName + '/' + subFolderName, (e) => {
+                if(e)
+                    isValid(false, e)
+                else
+                    isValid(fs.existsSync(subFolderPath));
+            })
+        });
     });
 })
