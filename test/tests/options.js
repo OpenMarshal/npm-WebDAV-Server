05330827cc8fbf6f2d949ed356e74232a43fdf18
Adrien Castex <adrien.castex@gmail.com>
2017-05-13 15:52:57 +0200

    Added test for OPTIONS method

---
+++ test/tests/options.js
@@ -0,0 +1,16 @@
+var webdav = require('../../lib/index.js'),
+    request = require('request')
+
+module.exports = (test, options, index) => test('options method', isValid =>
+{
+    var server = new webdav.WebDAVServer();
+    server.start(options.port + index);
+    isValid = isValid.multiple(1, server);
+
+    request({
+        url: 'http://localhost:' + (options.port + index),
+        method: 'OPTIONS'
+    }, (e, res, body) => {
+        isValid(res.headers.allow && res.headers.allow.length >= 3 && /^[a-zA-Z,]+$/g.test(res.headers.allow));
+    })
+})
