98a75e5f6ee3648a613135f172d3dc477636a113
Adrien Castex <adrien.castex@gmail.com>
2017-05-13 14:21:59 +0200

    Removed support for POST method

--- src/server/commands/Commands.ts
+++ src/server/commands/Commands.ts
@@ -1,13 +1,11 @@
 import NotImplemented from './NotImplemented'
 import Propfind from './Propfind'
 import Mkcol from './Mkcol'
-import Post from './Post'
 import Get from './Get'
 
 export default {
     NotImplemented,
     Propfind,
     Mkcol,
-    Post,
     Get
 }
--- src/server/commands/Post.ts
+++
@@ -1,2 +0,0 @@
-import * as Get from './Get'
-export default Get
