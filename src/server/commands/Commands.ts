3f11227a55fc8e4bf26c5534b0169b4a7c5bb997
Adrien Castex <adrien.castex@gmail.com>
2017-05-13 15:27:49 +0200

    Added PUT method

--- src/server/commands/Commands.ts
+++ src/server/commands/Commands.ts
@@ -1,11 +1,13 @@
 import NotImplemented from './NotImplemented'
 import Propfind from './Propfind'
 import Mkcol from './Mkcol'
+import Put from './Put'
 import Get from './Get'
 
 export default {
     NotImplemented,
     Propfind,
     Mkcol,
+    Put,
     Get
 }
---
+++ src/server/commands/Put.ts
@@ -0,0 +1,94 @@
+import { HTTPCodes, MethodCallArgs, WebDAVRequest } from '../WebDAVRequest'
+import { IResource, ResourceType } from '../../resource/Resource'
+import * as path from 'path'
+
+function createResource(arg : MethodCallArgs, callback, validCallback : (resource : IResource) => void)
+{
+    arg.server.getResourceFromPath(arg.path.getParent(), (e, r) => {
+        if(e)
+        {
+            arg.setCode(HTTPCodes.MethodNotAllowed)
+            callback()
+            return;
+        }
+        
+        if(!r.fsManager)
+        {
+            arg.setCode(HTTPCodes.InternalServerError)
+            callback();
+            return;
+        }
+        
+        const resource = r.fsManager.newResource(arg.uri, path.basename(arg.uri), ResourceType.File, r);
+        resource.create((e) => {
+            if(e)
+            {
+                arg.setCode(HTTPCodes.InternalServerError)
+                callback();
+                return;
+            }
+        
+            r.addChild(resource, (e) => {
+                if(e)
+                {
+                    arg.setCode(HTTPCodes.InternalServerError)
+                    callback();
+                }
+                else
+                    validCallback(resource);
+            })
+        })
+    })
+}
+
+export default function(arg : MethodCallArgs, callback)
+{
+
+    arg.getResource((e, r) => {
+        if(arg.contentLength === 0)
+        { // Create file
+            if(r)
+            { // Resource exists => empty it
+                r.write(new Buffer(0), (e) => {
+                    if(e)
+                        arg.setCode(HTTPCodes.InternalServerError)
+                    else
+                        arg.setCode(HTTPCodes.OK)
+                    callback()
+                })
+                return;
+            }
+            
+            createResource(arg, callback, (r) => {
+                arg.setCode(HTTPCodes.OK)
+                callback();
+            })
+        }
+        else
+        { // Write to a file
+            var data = new Buffer(arg.data);
+
+            if(e)
+            { // Resource not found
+                createResource(arg, callback, (r) => {
+                    r.write(data, (e) => {
+                        if(e)
+                            arg.setCode(HTTPCodes.InternalServerError)
+                        else
+                            arg.setCode(HTTPCodes.OK)
+                        callback();
+                    })
+                })
+                return;
+            }
+
+            r.write(data, (e) => {
+                if(e)
+                    arg.setCode(HTTPCodes.InternalServerError)
+                else
+                    arg.setCode(HTTPCodes.OK)
+                callback();
+            })
+        }
+    })
+}
