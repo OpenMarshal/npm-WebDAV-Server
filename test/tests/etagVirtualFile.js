6071bc90a7ef371f3af07c0b9c1f45f38174644d
Adrien Castex <adrien.castex@gmail.com>
2017-05-14 12:44:35 +0200

    Added tests ETag

---
+++ test/tests/etagVirtualFile.js
@@ -0,0 +1,74 @@
+var webdav = require('../../lib/index.js'),
+    Client = require('webdav-fs'),
+    request = require('request'),
+    xml2js = require('xml2js')
+
+module.exports = (test, options, index) => test('etag of virtual file', isValid =>
+{
+    var server = new webdav.WebDAVServer();
+    isValid = isValid.multiple(1, server);
+    server.rootResource.addChild(new webdav.VirtualFile('testFile.txt'), e => {
+        if(e)
+        {
+            isValid(false, e)
+            return;
+        }
+
+        server.start(options.port + index);
+
+        var wfs = Client(
+            "http://127.0.0.1:" + (options.port + index)
+        );
+
+        function propfind(callback)
+        {
+            request({
+                url: "http://127.0.0.1:" + (options.port + index) + '/testFile.txt',
+                method: 'PROPFIND'
+            }, (e, res, body) => {
+                if(e)
+                {
+                    isValid(false, e);
+                    return;
+                }
+
+                xml2js.parseString(body, (e, doc) => {
+                    if(e)
+                        isValid(false, e);
+                    else
+                        callback(doc);
+                });
+            })
+        }
+
+        propfind((doc) => {
+            const etag1 = doc['D:multistatus']['D:response'][0]['D:propstat'][0]['D:prop'][0]['D:getetag'][0];
+
+            propfind((doc) => {
+                const etag2 = doc['D:multistatus']['D:response'][0]['D:propstat'][0]['D:prop'][0]['D:getetag'][0];
+
+                if(etag1 !== etag2)
+                {
+                    isValid(false, 'ETag changed without file change');
+                    return;
+                }
+                
+                wfs.writeFile('/testFile.txt', 'New content', (e) => {
+                    if(e)
+                    {
+                        isValid(false, e);
+                        return;
+                    }
+
+                    propfind((doc) => {
+                        const etag3 = doc['D:multistatus']['D:response'][0]['D:propstat'][0]['D:prop'][0]['D:getetag'][0];
+                        if(etag1 === etag3)
+                            isValid(false, 'ETag didn\'t change with file change');
+                        else
+                            isValid(true);
+                    })
+                })
+            })
+        })
+    });
+})
--- test/tests/statVirtualFile.js
+++ test/tests/statVirtualFile.js
@@ -1,5 +1,7 @@
 var webdav = require('../../lib/index.js'),
-    Client = require("webdav-fs")
+    Client = require('webdav-fs'),
+    request = require('request'),
+    xml2js = require('xml2js')
 
 module.exports = (test, options, index) => test('stat of virtual file', isValid =>
 {
@@ -25,5 +27,26 @@ module.exports = (test, options, index) => test('stat of virtual file', isValid 
         wfs.stat('/notFoundFile.txt', (e, stat) => {
             isValid(!!e);
         })
+
+        function propfind(callback)
+        {
+            request({
+                url: "http://127.0.0.1:" + (options.port + index) + '/testFile.txt',
+                method: 'PROPFIND'
+            }, (e, res, body) => {
+                if(e)
+                {
+                    isValid(false, e);
+                    return;
+                }
+
+                xml2js.parseString(body, (e, doc) => {
+                    if(e)
+                        isValid(false, e);
+                    else
+                        callback(doc);
+                });
+            })
+        }
     });
 })
