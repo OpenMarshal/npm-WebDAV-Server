3ab05de8eb98d147f75a9d96c85487a8411f315b
Adrien Castex <adrien.castex@gmail.com>
2017-05-16 13:49:42 +0200

    Added tests for JSON and XML responses for PROPPATCH and PROPFIND

--- test/tests/proppatch.js
+++ test/tests/proppatch.js
@@ -8,24 +8,30 @@ module.exports = (test, options, index) => test('PROPPATCH method', isValid =>
 {
     var server = new webdav.WebDAVServer();
     server.start(options.port + index);
-    isValid = isValid.multiple(4, server);
+    isValid = isValid.multiple(4 * 2, server);
 
-    server.rootResource.addChild(new webdav.VirtualFile('testFile.txt'), test('testFile.txt'));
-    server.rootResource.addChild(new webdav.VirtualFolder('testFolder'), test('testFolder'));
+    testGroup('xml', false);
+    testGroup('json', true);
 
-    const pFileName = 'testPFile.txt';
-    const pFilePath = path.join(__dirname, 'proppatch', pFileName);
-    if(!fs.existsSync(pFilePath))
-        fs.writeFileSync(pFilePath, 'Content!');
-    server.rootResource.addChild(new webdav.PhysicalFile(pFilePath), test(pFileName));
+    function testGroup(prefix, isJSON)
+    {
+        server.rootResource.addChild(new webdav.VirtualFile(prefix + 'testFile.txt'), test(prefix + 'testFile.txt', isJSON));
+        server.rootResource.addChild(new webdav.VirtualFolder(prefix + 'testFolder'), test(prefix + 'testFolder', isJSON));
 
-    const pFolderName = 'testPFile.txt';
-    const pFolderPath = path.join(__dirname, 'proppatch', pFolderName);
-    if(!fs.existsSync(pFolderPath))
-        fs.writeFileSync(pFolderPath, 'Content!');
-    server.rootResource.addChild(new webdav.PhysicalFolder(pFolderPath), test(pFolderName));
+        const pFileName = prefix + 'testPFile.txt';
+        const pFilePath = path.join(__dirname, 'proppatch', pFileName);
+        if(!fs.existsSync(pFilePath))
+            fs.writeFileSync(pFilePath, 'Content!');
+        server.rootResource.addChild(new webdav.PhysicalFile(pFilePath), test(pFileName, isJSON));
 
-    function test(name)
+        const pFolderName = prefix + 'testPFile.txt';
+        const pFolderPath = path.join(__dirname, 'proppatch', pFolderName);
+        if(!fs.existsSync(pFolderPath))
+            fs.writeFileSync(pFolderPath, 'Content!');
+        server.rootResource.addChild(new webdav.PhysicalFolder(pFolderPath), test(pFolderName, isJSON));
+    }
+
+    function test(name, isJSON)
     {
         return (e) => {
             if(e)
@@ -35,12 +41,27 @@ module.exports = (test, options, index) => test('PROPPATCH method', isValid =>
             }
 
             const url = 'http://localhost:' + (options.port + index) + '/' + name;
+            
+            function tryCatch(callback)
+            {
+                try
+                {
+                    callback();
+                }
+                catch(e)
+                {
+                    isValid(false, 'Bad response body for ' + (isJSON ? 'JSON' : 'XML') + ' reponse.');
+                }
+            }
 
             // Add authors
             request({
                 url: url,
                 method: 'PROPPATCH',
-                body: '<?xml version="1.0" encoding="utf-8" ?><D:propertyupdate xmlns:D="DAV:" xmlns:Z="http://ns.example.com/standards/z39.50/"><D:set><D:prop><Z:Authors><Z:Author>Jim Whitehead</Z:Author><Z:Author>Roy Fielding</Z:Author></Z:Authors></D:prop></D:set></D:propertyupdate>'
+                body: '<?xml version="1.0" encoding="utf-8" ?><D:propertyupdate xmlns:D="DAV:" xmlns:Z="http://ns.example.com/standards/z39.50/"><D:set><D:prop><Z:Authors><Z:Author>Jim Whitehead</Z:Author><Z:Author>Roy Fielding</Z:Author></Z:Authors></D:prop></D:set></D:propertyupdate>',
+                headers: {
+                    Accept: isJSON ? 'application/json' : undefined
+                }
             }, (e, res, body) => {
                 if(e)
                 {
@@ -48,11 +69,10 @@ module.exports = (test, options, index) => test('PROPPATCH method', isValid =>
                     return;
                 }
 
-                const xml = xmljs.xml2js(body, { compact: true, alwaysArray: true });
-                const response = xml['D:multistatus'][0]['D:response'][0];
+                tryCatch(() => {
+                    const xml = isJSON ? JSON.parse(body) : xmljs.xml2js(body, { compact: true, alwaysArray: true });
+                    const response = xml['D:multistatus'][0]['D:response'][0];
                 
-                try
-                {
                     if(!(response['D:propstat'][0]['D:prop'][0]['x:Authors'].length === 1 &&
                         response['D:propstat'][0]['D:status'][0]._text[0].indexOf('HTTP/1.1 20') === 0 &&
                         response['D:href'][0]._text[0] === url))
@@ -63,7 +83,10 @@ module.exports = (test, options, index) => test('PROPPATCH method', isValid =>
                     
                     request({
                         url: url,
-                        method: 'PROPFIND'
+                        method: 'PROPFIND',
+                        headers: {
+                            Accept: isJSON ? 'application/json' : undefined
+                        }
                     }, (e, res, body) => {
                         if(e)
                         {
@@ -71,48 +94,55 @@ module.exports = (test, options, index) => test('PROPPATCH method', isValid =>
                             return;
                         }
                         
-                        const xml = xmljs.xml2js(body, { compact: true, alwaysArray: true });
-                        const prop = xml['D:multistatus'][0]['D:response'][0]['D:propstat'][0]['D:prop'][0];
+                        tryCatch(() => {
+                            const xml = isJSON ? JSON.parse(body) : xmljs.xml2js(body, { compact: true, alwaysArray: true });
+                            const prop = xml['D:multistatus'][0]['D:response'][0]['D:propstat'][0]['D:prop'][0];
 
-                        if(prop['x:Authors'].length !== 1)
-                        {
-                            isValid(false);
-                            return;
-                        }
-                        
-                        // Remove authors
-                        request({
-                            url: url,
-                            method: 'PROPPATCH',
-                            body: '<?xml version="1.0" encoding="utf-8" ?><D:propertyupdate xmlns:D="DAV:" xmlns:Z="http://ns.example.com/standards/z39.50/"><D:remove><D:prop><Z:Authors/></D:prop></D:remove></D:propertyupdate>'
-                        }, (e, res, body) => {
-                            if(e)
+                            if(prop['x:Authors'].length !== 1)
                             {
-                                isValid(false, e);
+                                isValid(false);
                                 return;
                             }
-
+                            
+                            // Remove authors
                             request({
                                 url: url,
-                                method: 'PROPFIND'
+                                method: 'PROPPATCH',
+                                body: '<?xml version="1.0" encoding="utf-8" ?><D:propertyupdate xmlns:D="DAV:" xmlns:Z="http://ns.example.com/standards/z39.50/"><D:remove><D:prop><Z:Authors/></D:prop></D:remove></D:propertyupdate>',
+                                headers: {
+                                    Accept: isJSON ? 'application/json' : undefined
+                                }
                             }, (e, res, body) => {
                                 if(e)
                                 {
                                     isValid(false, e);
                                     return;
                                 }
-                                const xml = xmljs.xml2js(body, { compact: true, alwaysArray: true });
-                                const prop = xml['D:multistatus'][0]['D:response'][0]['D:propstat'][0]['D:prop'][0];
 
-                                isValid(prop['x:Authors'] === undefined);
+                                request({
+                                    url: url,
+                                    method: 'PROPFIND',
+                                    headers: {
+                                        Accept: isJSON ? 'application/json' : undefined
+                                    }
+                                }, (e, res, body) => {
+                                    if(e)
+                                    {
+                                        isValid(false, e);
+                                        return;
+                                    }
+
+                                    tryCatch(() => {
+                                        const xml = isJSON ? JSON.parse(body) : xmljs.xml2js(body, { compact: true, alwaysArray: true });
+                                        const prop = xml['D:multistatus'][0]['D:response'][0]['D:propstat'][0]['D:prop'][0];
+
+                                        isValid(prop['x:Authors'] === undefined);
+                                    });
+                                });
                             });
                         });
                     });
-                }
-                catch(e)
-                {
-                    isValid(false, 'Bad response body.');
-                }
+                });
             })
         };
     }
