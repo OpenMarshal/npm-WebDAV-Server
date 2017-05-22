3cfdbeafacccfca779b3bf4f0fe762943927c9de
Adrien Castex <adrien.castex@gmail.com>
2017-05-22 12:18:42 +0200

    Added missing privilege check

--- src/server/commands/Get.ts
+++ src/server/commands/Get.ts
@@ -11,24 +11,26 @@ export default function(arg : MethodCallArgs, callback)
             return;
         }
 
-        r.read((e, c) => {
-            if(e)
-                arg.setCode(HTTPCodes.MethodNotAllowed)
-            else
-            {
-                arg.setCode(HTTPCodes.OK);
-
-                let content : any = c;
-                if(c === undefined || c === null)
-                    content = new Buffer(0);
-                else if(c.constructor === Boolean || c.constructor === Number)
-                    content = c.toString()
+        arg.requirePrivilege([ 'canRead' ], r, () => {
+            r.read((e, c) => {
+                if(e)
+                    arg.setCode(HTTPCodes.MethodNotAllowed);
                 else
-                    content = c;
-                
-                arg.response.write(content);
-            }
-            callback();
+                {
+                    arg.setCode(HTTPCodes.OK);
+
+                    let content : any = c;
+                    if(c === undefined || c === null)
+                        content = new Buffer(0);
+                    else if(c.constructor === Boolean || c.constructor === Number)
+                        content = c.toString()
+                    else
+                        content = c;
+                    
+                    arg.response.write(content);
+                }
+                callback();
+            })
         })
     })
 }
