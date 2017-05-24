516a7fb47413b6ac16fcca426f34c3291cc45535
Adrien Castex <adrien.castex@gmail.com>
2017-05-24 15:24:08 +0200

    Added more tests for the start method

--- test/tests/createServer.js
+++ test/tests/createServer.js
@@ -3,10 +3,22 @@ var webdav = require('../../lib/index.js');
 
 module.exports = (test, options, index) => test('create server', isValid =>
 {
-    var server = new webdav.WebDAVServer();
-    server.start(options.port + index);
+    var server = new webdav.WebDAVServer({
+        port: options.port + index
+    });
+    isValid = isValid.multiple(1, server);
 
-    server.stop(() => {
-        isValid(true);
-    })
+    server.start(options.port + index, (httpServer) => {
+        if(httpServer.address().port !== options.port + index)
+        {
+            isValid(false, 'Wrong port');
+            return;
+        }
+
+        server.stop(() => {
+            server.start((httpServer) => {
+                isValid(httpServer.address().port === options.port + index, 'Wrong port');
+            });
+        })
+    });
 })
