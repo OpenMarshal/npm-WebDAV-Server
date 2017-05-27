84cb9cda31c955cb3c1e7e721aa88fa035d6a75d
Adrien Castex <adrien.castex@gmail.com>
2017-05-27 10:57:37 +0200

    Fixed the RootFSManager's 'newResource' method to inherit a VirtualFSManager to created children instead of the RootFSManager itself

--- lib/manager/RootFSManager.d.ts
+++ lib/manager/RootFSManager.d.ts
@@ -1,8 +1,9 @@
-import { IResource } from '../resource/IResource';
-import { VirtualFSManager } from './VirtualFSManager';
+import { IResource, ResourceType } from '../resource/IResource';
 import { SerializedObject } from './ISerializer';
-export declare class RootFSManager extends VirtualFSManager {
+import { FSManager } from './FSManager';
+export declare class RootFSManager implements FSManager {
     uid: string;
     serialize(resource: any, obj: SerializedObject): object;
     unserialize(data: any, obj: SerializedObject): IResource;
+    newResource(fullPath: string, name: string, type: ResourceType, parent: IResource): IResource;
 }
--- src/manager/RootFSManager.ts
+++ src/manager/RootFSManager.ts
@@ -1,10 +1,15 @@
 import { IResource, ResourceType } from '../resource/IResource'
 import { VirtualFSManager } from './VirtualFSManager'
 import { SerializedObject } from './ISerializer'
+import { VirtualFolder } from '../resource/virtual/VirtualFolder'
 import { RootResource } from '../resource/std/RootResource'
+import { VirtualFile } from '../resource/virtual/VirtualFile'
 import { FSManager } from './FSManager'
+import { Errors } from '../Errors'
 
-export class RootFSManager extends VirtualFSManager
+const virtualFSManager = new VirtualFSManager();
+
+export class RootFSManager implements FSManager
 {
     uid : string = 'RootFSManager_1.0.2';
     
@@ -26,5 +31,15 @@ export class RootFSManager extends VirtualFSManager
         rs.lockBag.locks = data.locks;
         rs.properties = data.properties;
         return rs;
+    }
+
+    newResource(fullPath : string, name : string, type : ResourceType, parent : IResource) : IResource
+    {
+        if(type.isDirectory)
+            return new VirtualFolder(name, parent, virtualFSManager);
+        if(type.isFile)
+            return new VirtualFile(name, parent, virtualFSManager);
+
+        throw Errors.UnrecognizedResource;
     }
 }
