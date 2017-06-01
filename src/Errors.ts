1daac87d1f02a99e6519de8bc1a3b0251e82800d
Adrien Castex <adrien.castex@protonmail.com>
2017-06-01 15:04:53 +0200

    Added a dot (.) at the end of the text of each common error

--- src/Errors.ts
+++ src/Errors.ts
@@ -16,24 +16,24 @@ export class HTTPError extends Error
 }
 
 export const Errors = {
-    BadAuthentication: new Error('Bad authentication'),
-    AuenticationPropertyMissing: new Error('Properties are missing'),
-    WrongHeaderFormat: new Error('Wrong header format'),
-    MissingAuthorisationHeader: new Error('Missing Authorization header'),
-    UnrecognizedResource: new Error('Unrecognized resource'),
-    ParentPropertiesMissing: new Error('The parent resource must have some special properties'),
-    InvalidOperation: new Error('Invalid operation'),
+    BadAuthentication: new Error('Bad authentication.'),
+    AuenticationPropertyMissing: new Error('Properties are missing.'),
+    WrongHeaderFormat: new Error('Wrong header format.'),
+    MissingAuthorisationHeader: new Error('Missing Authorization header.'),
+    UnrecognizedResource: new Error('Unrecognized resource.'),
+    ParentPropertiesMissing: new Error('The parent resource must have some special properties.'),
+    InvalidOperation: new Error('Invalid operation.'),
     ResourceAlreadyExists: new Error('The resource already exists.'),
     ResourceNotFound: new Error('Can\'t find the resource.'),
     CannotLockResource: new Error('Can\'t lock the resource.'),
     PropertyNotFound: new Error('No property with such name.'),
-    AlreadyAuthenticated: new Error('Already authenticated'),
-    UserNotFound: new Error('User not found'),
+    AlreadyAuthenticated: new Error('Already authenticated.'),
+    UserNotFound: new Error('User not found.'),
     XMLNotFound: new Error('Can\'t find the XML element.'),
     ExpectedAFileResourceType: new Error('Expected a file resource type.'),
-    NoMimeTypeForAFolder: new Error('Cannot get the mime type of a folder type resource'),
-    NoSizeForAFolder: new Error('Cannot get the size of a folder type resource'),
-    IllegalArguments: new Error('Illegal arguments'),
+    NoMimeTypeForAFolder: new Error('Cannot get the mime type of a folder type resource.'),
+    NoSizeForAFolder: new Error('Cannot get the size of a folder type resource.'),
+    IllegalArguments: new Error('Illegal arguments.'),
 
     None: null
 };
