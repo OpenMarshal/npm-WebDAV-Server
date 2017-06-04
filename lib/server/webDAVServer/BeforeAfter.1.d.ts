e2e054cba9a54110cec833756d3da10d742956f1
Adrien Castex <adrien.castex@protonmail.com>
2017-06-04 13:41:15 +0200

    Cleaned the 'lib' folder

--- lib/server/webDAVServer/BeforeAfter.1.d.ts
+++
@@ -1,6 +0,0 @@
-import { MethodCallArgs, WebDAVRequest } from '../WebDAVRequest';
-export declare function beforeRequest(manager: WebDAVRequest): void;
-export declare function afterRequest(manager: WebDAVRequest): void;
-export declare function invokeBARequest(collection: WebDAVRequest[], base: MethodCallArgs, callback: any): void;
-export declare function invokeBeforeRequest(base: MethodCallArgs, callback: any): void;
-export declare function invokeAfterRequest(base: MethodCallArgs, callback: any): void;
