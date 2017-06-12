b6548aa06ce3e0f2146d3cafc2c5793e918d4850
Adrien Castex <adrien.castex@protonmail.com>
2017-06-12 16:34:17 +0200

    Added the export of the 'SimplePathPrivilegeManager' class

--- lib/user/export.d.ts
+++ lib/user/export.d.ts
@@ -1,6 +1,7 @@
 export * from './authentication/HTTPDigestAuthentication';
 export * from './authentication/HTTPBasicAuthentication';
 export * from './authentication/HTTPAuthentication';
+export * from './privilege/SimplePathPrivilegeManager';
 export * from './privilege/SimplePrivilegeManager';
 export * from './privilege/FakePrivilegeManager';
 export * from './privilege/IPrivilegeManager';
