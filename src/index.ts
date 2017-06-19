585b1727e10c174a564e051843c09cbd3992de96
Adrien Castex <adrien.castex@protonmail.com>
2017-06-19 15:14:55 +0200

    Added the export of the 'helper' classes to ease the creation of custom fsManagers/resources

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ export * from './manager/export';
 export * from './server/export';
 export * from './resource/export';
 export * from './user/export';
+export * from './helper/export';
 export * from './Errors';
