d0aa8c84e577986907188a7396d20faf755c4417
Adrien Castex <adrien.castex@protonmail.com>
2017-06-20 14:29:40 +0200

    Implemented the physical gateway and its FSManager

---
+++ lib/resource/physical/PhysicalGateway.d.ts
@@ -0,0 +1,16 @@
+import { IResource, ReturnCallback } from '../IResource';
+import { FSManager, FSPath } from '../../manager/FSManager';
+import { PhysicalResource } from './PhysicalResource';
+import { MethodCallArgs } from '../../server/MethodCallArgs';
+import { PhysicalFolder } from './PhysicalFolder';
+export declare class PhysicalGateway extends PhysicalFolder {
+    protected customName: string;
+    cache: {
+        [path: string]: PhysicalResource;
+    };
+    constructor(rootPath: string, customName?: string, parent?: IResource, fsManager?: FSManager);
+    webName(callback: ReturnCallback<string>): void;
+    protected listChildren(parent: PhysicalResource, rpath: string, callback: (error: Error, children?: IResource[]) => void): void;
+    protected find(path: FSPath, callback: (error: Error, resource?: PhysicalResource) => void, forceRefresh?: boolean): void;
+    gateway(arg: MethodCallArgs, path: FSPath, callback: (error: Error, resource?: IResource) => void): void;
+}
