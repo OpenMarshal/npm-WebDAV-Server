9ce23d1a988003dbd0e2936ce81c3f74f02c6030
Adrien Castex <adrien.castex@protonmail.com>
2017-06-26 12:26:51 +0200

    Added tests for the readDir/propfind on a directory (v2)

---
+++ test/v2/tests.ts/readDir/.createDir.ts
@@ -0,0 +1,29 @@
+import { TestCallback, TestInfo } from '../Type'
+import { v2 } from '../../../../lib/index.js'
+
+export function starter(server : v2.WebDAVServer, info : TestInfo, isValid : TestCallback, callback : (r : v2.Resource, subFiles ?: string[]) => void) : void
+{
+    const subFiles = [
+        'subFolder1',
+        'subFolder2',
+        'subFile1',
+        'subFile2'
+    ];
+    const name = 'folder';
+    server.rootFileSystem.addSubTree(info.ctx, {
+        [name]: {
+            'subFolder1': v2.ResourceType.Directory,
+            'subFolder2': v2.ResourceType.Directory,
+            'subFile1': v2.ResourceType.File,
+            'subFile2': v2.ResourceType.File,
+        }
+    }, (e) => {
+        if(e) return isValid(false, 'Cannot call "addSubTree(...)".', e);
+
+        server.getResource(info.ctx, '/' + name, (e, r) => {
+            if(e) return isValid(false, 'Could not find /' + name, e);
+
+            callback(r, subFiles);
+        })
+    })
+}
---
+++ test/v2/tests.ts/readDir/propfindDirectory.ts
@@ -0,0 +1,60 @@
+import { Test } from '../Type'
+import { v2 } from '../../../../lib/index.js'
+import { starter } from './.createDir'
+
+export default ((info, isValid) =>
+{
+    const server1 = info.init(2);
+    
+    starter(server1, info, isValid, (r, subFiles) => {
+        info.reqXML({
+            url: 'http://localhost:' + info.port + '/folder',
+            method: 'PROPFIND',
+            headers: {
+                depth: 1
+            }
+        }, v2.HTTPCodes.MultiStatus, (req, body) => {
+            try
+            {
+                const sub = body.find('DAV:multistatus').findMany('DAV:response').map((r) => r.find('DAV:propstat').find('DAV:prop').find('DAV:displayname').findText());
+                subFiles.push('folder');
+
+                for(const sf of sub)
+                {
+                    const index = subFiles.indexOf(sf);
+                    if(index === -1)
+                        return isValid(false, 'Got a file name in "readDir(...)" which must not exist here : ' + sf);
+                    
+                    delete subFiles[index];
+                }
+
+                isValid(subFiles.length > 0, 'All children were not returned ; here are the left ones : ' + subFiles.toString());
+            }
+            catch(ex)
+            {
+                isValid(false, 'Invalid WebDAV response body.', ex);
+            }
+        })
+    })
+
+    const server2 = info.startServer();
+    starter(server2, info, isValid, (r, subFiles) => {
+        info.reqXML({
+            url: 'http://localhost:' + info.port + '/folder',
+            method: 'PROPFIND',
+            headers: {
+                depth: 0
+            }
+        }, v2.HTTPCodes.MultiStatus, (req, body) => {
+            try
+            {
+                isValid(body.find('DAV:multistatus').findMany('DAV:response').length === 1, 'Too many or not enought DAV:response tags.');
+            }
+            catch(ex)
+            {
+                isValid(false, 'Invalid WebDAV response body.', ex);
+            }
+        })
+    })
+
+}) as Test;
