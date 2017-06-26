735e2ed8afd4c173e4a15afaf85fa7337b6ff3bf
Adrien Castex <adrien.castex@protonmail.com>
2017-06-26 12:26:00 +0200

    Added tests for the server management (v2)

---
+++ test/v2/tests.ts/server/startAndStop.ts
@@ -0,0 +1,22 @@
+import { Test } from '../Type'
+import { v2 } from '../../../../lib/index.js'
+import * as request from 'request'
+
+export default ((info, isValid) =>
+{
+    const server = info.init(1);
+    const server2 = info.startServer({}, false);
+
+    server.stop(() => {
+        server2.start(info.port + 4, (httpServer) => {
+            if(httpServer.address().port !== info.port + 4)
+                return isValid(false, 'Wrong port');
+
+            server2.stop(() => {
+                server2.start((httpServer) => {
+                    isValid(httpServer.address().port === info.port + 1, 'Wrong port');
+                });
+            })
+        });
+    });
+}) as Test;
