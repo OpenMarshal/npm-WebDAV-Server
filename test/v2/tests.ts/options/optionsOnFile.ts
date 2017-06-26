6e5981a68570bcbb754d813d9e621e90d342987a
Adrien Castex <adrien.castex@protonmail.com>
2017-06-26 12:27:07 +0200

    Added tests for the OPTIONS method (v2)

---
+++ test/v2/tests.ts/options/optionsOnFile.ts
@@ -0,0 +1,17 @@
+import { Test } from '../Type'
+import { v2 } from '../../../../lib/index.js'
+import { starter } from './.createFiles'
+
+export default ((info, isValid) =>
+{
+    const server = info.init(1);
+    
+    starter(server, info, isValid, 'file', [
+        'GET', 'PUT', 'POST', 'COPY', 'DELETE', 'HEAD', 'LOCK', 'MOVE', 'PROPFIND', 'PROPPATCH', 'UNLOCK'
+    ], [
+        'MKCOL'
+    ], () => {
+        isValid(true);
+    })
+
+}) as Test;
