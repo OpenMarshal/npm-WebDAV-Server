5c041256dc2bb03d528a52bbfdc9118e6e9ef5ac
Adrien Castex <adrien.castex@protonmail.com>
2017-06-28 17:23:41 +0200

    Moved the 'FTPFileSystem' class to the '@webdav-server/ftp' package

--- lib/manager/v2/export.d.ts
+++ lib/manager/v2/export.d.ts
@@ -1,4 +1,3 @@
-export * from './instances/FTPFileSystem';
 export * from './instances/PhysicalFileSystem';
 export * from './instances/VirtualFileSystem';
 export * from './fileSystem/export';
--- src/manager/v2/export.ts
+++ src/manager/v2/export.ts
@@ -1,5 +1,4 @@
 
-export * from './instances/FTPFileSystem'
 export * from './instances/PhysicalFileSystem'
 export * from './instances/VirtualFileSystem'
 export * from './fileSystem/export'
