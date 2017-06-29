f14cde3dd1409a747eaa99afff71dd4465fb7f64
Adrien Castex <adrien.castex@protonmail.com>
2017-06-29 17:39:57 +0200

    Added the reverse operation in the PROPPATCH method to fit the RFC (1 error => reverse changes)

--- src/server/v2/commands/Proppatch.ts
+++ src/server/v2/commands/Proppatch.ts
@@ -1,8 +1,9 @@
 import { HTTPCodes, HTTPMethod, RequestContext } from '../WebDAVRequest'
 import { ResourceType } from '../../../manager/v2/fileSystem/CommonTypes'
+import { IPropertyManager } from '../../../manager/v2/fileSystem/PropertyManager'
 import { STATUS_CODES } from 'http'
 import { Workflow } from '../../../helper/Workflow'
-import { XML } from '../../../helper/XML'
+import { XML, XMLElement } from '../../../helper/XML'
 import { Errors } from '../../../Errors'
 
 export default class implements HTTPMethod
@@ -22,23 +23,57 @@ export default class implements HTTPMethod
                     {
                         const xml = XML.parse(data);
                         const root = xml.find('DAV:propertyupdate');
+                        const notifications : any = { }
+                        const reverse = [];
 
                         let finalize = function()
                         {
                             finalize = function()
                             {
-                                ctx.setCode(HTTPCodes.MultiStatus);
-                                ctx.writeBody(multistatus);
-                                callback();
+                                const next = () => {
+                                    const codes = Object.keys(notifications);
+
+                                    codes.forEach((code) => {
+                                        const propstat = response.ele('D:propstat');
+                                        const prop = propstat.ele('D:prop');
+                                        notifications[code].forEach((name) => prop.ele(name));
+                                        propstat.ele('D:status').add('HTTP/1.1 ' + code + ' ' + STATUS_CODES[code]);
+                                    })
+
+                                    ctx.setCode(HTTPCodes.MultiStatus);
+                                    ctx.writeBody(multistatus);
+                                    callback();
+                                }
+
+                                if(Object.keys(notifications).length > 1)
+                                {
+                                    new Workflow()
+                                        .each(reverse, (action, cb) => action(cb))
+                                        .error((e) => {
+                                            if(!ctx.setCodeFromError(e))
+                                                ctx.setCode(HTTPCodes.InternalServerError)
+                                            callback();
+                                        })
+                                        .done(() => {
+                                            if(notifications[HTTPCodes.OK])
+                                            {
+                                                notifications[HTTPCodes.FailedDependency] = notifications[HTTPCodes.OK];
+                                                delete notifications[HTTPCodes.OK];
+                                            }
+                                            next();
+                                        })
+                                }
+                                else
+                                    next();
                             }
                         }
 
                         const notify = function(el : any, error : any)
                         {
                             const code = error ? HTTPCodes.Forbidden : HTTPCodes.OK;
-                            const propstat = response.ele('D:propstat');
-                            propstat.ele('D:prop').ele(el.name);
-                            propstat.ele('D:status').add('HTTP/1.1 ' + code + ' ' + STATUS_CODES[code]);
+                            if(!notifications[code])
+                                notifications[code] = [];
+                            notifications[code].push(el.name);
                         }
 
                         const execute = function(name : string, eventName : /*EventsName*/string, fnProp)
@@ -54,11 +89,16 @@ export default class implements HTTPMethod
                                 const els = el.find('DAV:prop').elements;
                                 
                                 new Workflow(false)
-                                    .each(els, fnProp)
+                                    .each(els, (x, cb) => {
+                                        if(x.type !== 'element')
+                                            return cb();
+                                        fnProp(x, cb);
+                                    })
                                     .intermediate((el, e) => {
                                         /*if(!e)
                                             ctx.invokeEvent(eventName, r, el)*/
-                                        notify(el, e)
+                                        if(el.type === 'element')
+                                            notify(el, e)
                                     })
                                     .done(() => finalize())
                             })
@@ -85,12 +125,54 @@ export default class implements HTTPMethod
                                     return callback();
                                 }
 
-                                execute('DAV:set', 'setProperty', (el, callback) => {
-                                    pm.setProperty(el.name, el.elements, callback)
-                                })
-                                execute('DAV:remove', 'removeProperty', (el, callback) => {
-                                    pm.removeProperty(el.name, callback)
-                                })
+                                pm.getProperties((e, props) => {
+                                    if(e)
+                                    {
+                                        if(!ctx.setCodeFromError(e))
+                                            ctx.setCode(HTTPCodes.InternalServerError)
+                                        return callback();
+                                    }
+
+                                    const properties = JSON.parse(JSON.stringify(props));
+
+                                    const pushSetReverseAction = (el : XMLElement) => {
+                                        const prop = properties[el.name];
+                                        if(prop)
+                                            reverse.push((cb) => pm.setProperty(el.name, prop.value, prop.attributes, cb));
+                                        else
+                                            reverse.push((cb) => pm.removeProperty(el.name, cb));
+                                    }
+                                    const pushRemoveReverseAction = (el : XMLElement) => {
+                                        const prop = properties[el.name];
+                                        reverse.push((cb) => pm.setProperty(el.name, prop.value, prop.attributes, cb));
+                                    }
+                                    execute('DAV:set', 'setProperty', (el : XMLElement, callback) => {
+                                        if(el.name.indexOf('DAV:') === 0)
+                                        {
+                                            pushSetReverseAction(el);
+                                            return callback(Errors.Forbidden);
+                                        }
+
+                                        pm.setProperty(el.name, el.elements, el.attributes, (e) => {
+                                            if(!e)
+                                                pushSetReverseAction(el);
+                                            callback(e);
+                                        })
+                                    })
+                                    execute('DAV:remove', 'removeProperty', (el : XMLElement, callback) => {
+                                        if(el.name.indexOf('DAV:') === 0)
+                                        {
+                                            pushRemoveReverseAction(el);
+                                            return callback(Errors.Forbidden);
+                                        }
+
+                                        pm.removeProperty(el.name, (e) => {
+                                            if(!e)
+                                                pushRemoveReverseAction(el);
+                                            callback(e);
+                                        })
+                                    })
+                                }, false)
                             })
                         })
                     }
