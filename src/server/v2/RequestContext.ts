c12096a04f71559064746c18f7289168656d2708
Adrien Castex <adrien.castex@protonmail.com>
2017-07-31 17:15:19 +0200

    Fixed wrong content length when the content contains special characters + Specified the encoding when writing to the response body (UTF-8) + Minimified the 'Content-Type' header in the 'writeBody' method of the 'RequestContext' class

--- src/server/v2/RequestContext.ts
+++ src/server/v2/RequestContext.ts
@@ -332,16 +332,16 @@ export class HTTPRequestContext extends RequestContext
         {
             default:
             case 'xml':
-                this.response.setHeader('Content-Type', 'application/xml; charset="utf-8"');
-                this.response.setHeader('Content-Length', content.length.toString());
-                this.response.write(content);
+                this.response.setHeader('Content-Type', 'application/xml;charset=utf-8');
+                this.response.setHeader('Content-Length', new Buffer(content).length.toString());
+                this.response.write(content, 'UTF-8');
                 break;
                 
             case 'json':
                 content = XML.toJSON(content);
-                this.response.setHeader('Content-Type', 'application/json; charset="utf-8"');
-                this.response.setHeader('Content-Length', content.length.toString());
-                this.response.write(content);
+                this.response.setHeader('Content-Type', 'application/json;charset=utf-8');
+                this.response.setHeader('Content-Length', new Buffer(content).length.toString());
+                this.response.write(content, 'UTF-8');
                 break;
         }
 
