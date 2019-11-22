b31955d214dd74177d799d0fb1413190d53c875f
Adrien Castex <adrien.castex@gmail.com>
2019-11-22 13:32:19 +0100

    Fixed compilations (src and tests) for node version 4.9

--- test/v2/tests/delete/deleteOnUndefined.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_js_1 = require("../../../../lib/index.js");
-var _createFiles_1 = require("./.createFiles");
-exports.default = (function (info, isValid) {
-    var server1 = info.init(1);
-    _createFiles_1.starter(info.startServer(), info, isValid, 'fileUndefined', index_js_1.v2.HTTPCodes.NotFound);
-});
